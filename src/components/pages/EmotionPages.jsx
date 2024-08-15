fe44a484900a3114f79bcb0eaae83924808fd433
Le Minh Trung <trunggaming65@gmail.com>
2024-08-15 22:32:23 +0700

    use TailWind fix random song

--- src/components/pages/EmotionPages.jsx
+++ src/components/pages/EmotionPages.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useFetchers, useLocation } from "react-router-dom";
 import { getApiSongEmotion } from "../../services/UserServices";
-import "./EmotionPages.css";
 import video from "../../assets/vbg_happy.mp4";
 
 const HappyEmotion = () => {
@@ -13,33 +12,51 @@ const HappyEmotion = () => {
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
     const emotionParam = queryParams.get("emotion");
+    console.log(emotionParam);
     setEmotion(emotionParam);
+  }, [location.search])
 
-    if (emotionParam) {
-      getApiSongEmotion(emotionParam)
-        .then((response) => {
+  useEffect(() => {
+    // if (emotion) {
+    //   getApiSongEmotion(emotion)
+    //     .then((response) => {
+    //       const songDisplay = response.data;
+    //       setSongs(songDisplay);
+
+    //       const randomIndex = Math.floor(Math.random() * songDisplay.length);
+    //       setRandomSong(songDisplay[randomIndex]);
+    //     })
+    //     .catch((error) => {
+    //       console.error("There was an error!", error);
+    //     });
+    // }
+    if (emotion) {
+      const fetchVideo = async () => {
+        try {
+          const response = await getApiSongEmotion(emotion);
           const songDisplay = response.data;
           setSongs(songDisplay);
 
           const randomIndex = Math.floor(Math.random() * songDisplay.length);
           setRandomSong(songDisplay[randomIndex]);
-        })
-        .catch((error) => {
-          console.error("There was an error!", error);
-        });
-    }
-  }, [location.search]);
+        } catch (error) {
+          console.log("There was an error!", error);
+        }
+      }
+      fetchVideo()
+    };
+  }, [emotion]);
 
   console.log(randomSong);
 
   return (
     <div className="containerEmotion">
-      <video autoPlay loop muted playsInline className="backVideo">
+      <video autoPlay loop muted playsInline className="absolute right-0 bottom-0 z-[-1] object-cover">
         <source src={video} type="video/mp4" />
       </video>
       {randomSong ? (
         <iframe
-          className="videoSong"
+          className="absolute left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%] w-[1000px] h-[500px]"
           width="600"
           height="315"
           src={`https://www.youtube.com/embed/${randomSong.yt_id}`}
--- src/components/profileUser/ProfileUser.jsx
+++ src/components/profileUser/ProfileUser.jsx
@@ -1,4 +1,3 @@
-import "./ProfileUser.css";
 import toast from "react-hot-toast";
 import { getUserInfo } from "../../services/UserServices";
 import { useEffect, useState } from "react";
@@ -31,16 +30,17 @@ function ProfileUser() {
   }, []);
 
   return (
-    <div className="containerPro">
-      <div className="imgProfile">
+    <div className="w-[500px] h-[700px] block my-[50px] mx-auto bg-[rgb(15,15,15)] rounded-[50px] text-center font-josefinSans text-[20px] text-white">
+      <div className="block w-[150px] h-[150px] my-[50px] mx-auto pt-[20px]">
         <img
           src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t1.6435-9/45901944_964430937078341_8738149665662304256_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=7b2446&_nc_ohc=lbYhqHgvcUIQ7kNvgE9rkr7&_nc_ht=scontent.fsgn2-4.fna&oh=00_AYCEkKTYluzupDfqyQfk3bNWd376BokrIWPksYKnbLJtXQ&oe=66C5D602"
           alt="ImageAVT"
+          className="rounded-[50%] w-[150px] h-[150px] object-cover mt-[50px]"
         />
       </div>
-      <p className="roleProfile">Member</p>
-      <p className="nameProfile">{nameUser}</p>
-      <p className="nameProfile">{emailUser}</p>
+      <p className="mb-[50px] mt-[120px]">Member</p>
+      <p className="text-white">{nameUser}</p>
+      <p className="text-white">{emailUser}</p>
     </div>
   );
 }
